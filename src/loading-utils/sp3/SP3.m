1ba9e2c0509f57f3e5035bf732a156c5b6ce2868
spanikp <petospanik@gmail.com>
2019-08-09 01:02:26 +0200

    Start work on SP3 merging

--- src/loading-utils/sp3/SP3.m
+++ src/loading-utils/sp3/SP3.m
@@ -2,96 +2,92 @@ classdef SP3
     properties
         fileList (1,1) fileList
         header (1,:) SP3header
+        gnss
         t (:,9) double
         sat
+        pos
+        clockData
+        satTimeFlags
     end
     
     methods
         function obj = SP3(cellFilelist)
             obj.fileList = fileList(cellFilelist,{'SP3','sp3','eph','EPH'});
+            temp = struct('pos',[],'t',[],'sat',[],'clockData',[],'satTimeFlags',[]);
             for i = 1:numel(obj.fileList.fileNames)
                 f = obj.fileList.fileNames{i};
                 obj.header(i) = SP3header(f);
-                
+                [temp(i).pos,temp(i).t,temp(i).sat,temp(i).clockData,temp(i).satTimeFlags] = SP3.loadContent(f);
+                if ~isequal(temp(i).sat,obj.header(i).sat)
+                   warning('Inconsistency between HEADER and BODY satellite list for "%s" file!',obj.header(i).filename) 
+                end
             end
+            %[obj.pos, obj.t, obj.sat, obj.clockData, obj.satTimeFlags] = SP3.mergeContent(temp);
         end
+%         function obj = resample(obj,everyNepoch)
+% 
+%         end
     end
     methods (Static)
-        function sp3 = loadSP3(filename)
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Function to read content of SP3 file.
-        %
-        % Input:
-        % filename - input filename (single file in SP3 format)
-        % Output:
-        % sp3 - structure with the following fields:
-        %    .gnss - satellite system identifiers
-        %    .t - [epochs x 9] array of time moments with following content:
-        %         [year, month, day, hour, minute, second, GPSWeek, GPSSecond, mTime]
-        %    .sat.(gnss) - [1 x n] array of PRN numbers
-        %        .satpos.(gnss) - [n x 4] array of [X, Y, Z, dT]
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        validateattributes(filename,{'char'},{'size',[1,NaN]},1);
-
-        finp = fopen(filename,'r');
-        raw = textscan(finp,'%s','Delimiter','\n','Whitespace', '');
-        raw = raw{1};
-        fclose(finp);
-        
-        % Skip header
-        lindex = 0;
-        systems = '';
-        while 1
-            lindex = lindex + 1;
-            line = raw{lindex};
-            if strcmp(line(1),'+')
-                systems = [systems, line];
-            end
-            if strcmp(line(1),'*')
-                break;
-            end
-        end
-        
-        % Find out number of epochs in file
-        raw = raw(lindex:end);
-        selEpochs = cellfun(@(x) strcmp(x(1),'*'),raw);
-        ymdhms = cell2mat(cellfun(@(x) sscanf(x(2:end),'%f')', raw(selEpochs),'UniformOutput',false));
-        [gpsweek, tow, ~, ~] = greg2gps(ymdhms);
-        sp3.t = [ymdhms, gpsweek, tow, datenum(ymdhms)];
-        
-        % Allocate structure fields for different systems
-        gnss = unique(systems(regexp(systems,'[A-Z]')));
-        sp3.gnss = gnss;
-        for i = 1:length(gnss)
-            sp3.satpos.(gnss(i)) = cell(1,32);
-            sp3.satpos.(gnss(i))(:) = {zeros(size(sp3.t,1),4)};
-        end
-        
-        % Looping through the file
-        idxEpoch = 0;
-        for i = 1:length(raw)
-            if contains(raw{i},'EOF')
-                break;
+        function [pos, t, sat, clockData, satTimeFlags] = loadContent(filename)
+            validateattributes(filename,{'char'},{'size',[1,NaN]},1);
+            finp = fopen(filename,'r');
+            raw = textscan(finp,'%s','Delimiter','\n','Whitespace', '');
+            raw = raw{1};
+            fclose(finp);
+            
+            % Get epochs
+            selEpochs = cellfun(@(x) strcmp(x(1),'*'),raw);
+            contenStartIdx = find(selEpochs);
+            contenStartIdx = contenStartIdx(1);
+            ymdhms = cell2mat(cellfun(@(x) sscanf(x(2:end),'%f')', raw(selEpochs),'UniformOutput',false));
+            [gpsweek, tow, ~, ~] = greg2gps(ymdhms);
+            t = [ymdhms, gpsweek, tow, datenum(ymdhms)];
+            raw = raw(contenStartIdx:end);
+            
+            % Allocate structure fields for different systems
+            satsys = cellfun(@(x) x(2),raw(1:end-1))';
+            gnss = strtrim(unique(satsys));
+            for i = 1:numel(gnss)
+                selgnss = cellfun(@(x) strcmp(x(2),gnss(i)),raw);
+                sat.(gnss(i)) = unique(cellfun(@(x) str2double(x(3:4)),raw(selgnss))');
+                pos.(gnss(i)) = cell(1,numel(sat.(gnss(i))));
+                pos.(gnss(i))(:) = {zeros(size(t,1),3)};
+                clockData.(gnss(i)) = cell(1,numel(sat.(gnss(i))));
+                clockData.(gnss(i))(:) = {NaN(size(t,1),1)};
+                satTimeFlags.(gnss(i)) = false(size(t,1),numel(sat.(gnss(i))));
             end
             
-            line = raw{i};
-            if strcmp(line(1),'*')
-                idxEpoch = idxEpoch + 1;
-            else
-                prn = sscanf(line(3:4),'%f');
-                xyzc = sscanf(line(5:60),'%f')';
-                xyzc(:,1:3) = xyzc(:,1:3)*1000;
-                sp3.satpos.(line(2)){prn}(idxEpoch,:) = xyzc;
+            % Looping through the file
+            idxEpoch = 0;
+            for i = 1:length(raw)
+                line = raw{i};
+                if strcmp(line(1),'*')
+                    idxEpoch = idxEpoch + 1;
+                elseif strcmp(line(1),'P')
+                    prn = sscanf(line(3:4),'%f');
+                    xyzc = sscanf(line(5:60),'%f')';
+                    idxSat = find(sat.(line(2)) == prn);
+                    pos.(line(2)){idxSat}(idxEpoch,:) = xyzc(1:3)*1e3;
+                    if round(xyzc(4)) ~= 1e6
+                        clockData.(line(2)){idxSat}(idxEpoch,:) = xyzc(4);
+                    end
+                    satTimeFlags.(line(2))(idxEpoch,idxSat) = true;
+                else
+                    continue
+                end
             end
         end
-        
-        % Remove cells without records
-        allSats = 1:32;
-        for i = 1:length(gnss)
-            satSel = cellfun(@(x) sum(sum(x))~=0, sp3.satpos.(gnss(i)));
-            sp3.sat.(gnss(i)) = allSats(satSel);
-            sp3.satpos.(gnss(i))(~satSel) = [];
-        end
+        function [pos, t, sat, clockData, satTimeFlags] = mergeContent(contentStruct)
+            diffFields = setdiff(fieldnames(contentStruct),{'pos','t','sat','clockData','satTimeFlags'}');
+            if ~isempty(diffFields)
+                error('Input content structure is missing fields: %s',strjoin(diffFields,','));
+            else
+                gnss = '';
+                for i = 1:numel(contentStruct)
+                    
+                end
+            end
         end
 	end
 end
