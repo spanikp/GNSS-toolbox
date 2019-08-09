1c7d6503cc4055f0683b49dad73cbbf871dec58f
spanikp <petospanik@gmail.com>
2019-08-09 22:33:43 +0200

    Fixed getLeapSeconds.m

--- src/time-utils/getLeapSeconds.m
+++ src/time-utils/getLeapSeconds.m
@@ -1,9 +1,7 @@
 function leapSeconds = getLeapSeconds(gpstime)
 % leapSeconds seconds is number of between the GPS Epoch and utcTime(i,:)
 % See IERS Bulletin C, https://hpiers.obspm.fr/iers/bul/bulc/bulletinc.dat  
-
 leapSeconds = NaN(size(gpstime,1),1);
-
 utcTable = [1982 1 1 0 0 0;
             1982 7 1 0 0 0;
             1983 7 1 0 0 0;
@@ -22,3 +20,9 @@ utcTable = [1982 1 1 0 0 0;
             2012 7 1 0 0 0;
             2015 7 1 0 0 0;
             2017 1 1 0 0 0];
+
+mtimeGiven = gps2matlabtime(gpstime);
+mtimeJumps = datenum(utcTable);
+for i = 1:numel(mtimeGiven)
+    leapSeconds(i) = find((mtimeGiven(i) - mtimeJumps) > 0,1,'last');
+end
