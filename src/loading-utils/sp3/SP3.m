d0ff1a9943011b8e6c32f0fb66c1748f315d99d8
spanikp <petospanik@gmail.com>
2019-08-10 16:02:20 +0200

    Allocat x,y,z for speedup

--- src/loading-utils/sp3/SP3.m
+++ src/loading-utils/sp3/SP3.m
@@ -64,14 +64,17 @@ classdef SP3
         end
         function [x,y,z] = interpolatePosition(obj,satsys,prn,mtime)
             prnIdx = find(obj.sat.(satsys) == prn);
+            x = zeros(numel(mtime),1);
+            y = zeros(numel(mtime),1);
+            z = zeros(numel(mtime),1);
             if ~isempty(prnIdx)
                 for i = 1:numel(mtime)
                     midIdx = find(obj.t(:,9) - mtime(i) >= 0,1,'first');
                     idxStart = midIdx - 5;
                     idxEnd = midIdx + 4;
-                    x = lagrange(obj.t(idxStart:idxEnd,9),obj.pos.(satsys){prnIdx}(idxStart:idxEnd,1),mtime(i));
-                    y = lagrange(obj.t(idxStart:idxEnd,9),obj.pos.(satsys){prnIdx}(idxStart:idxEnd,2),mtime(i)); 
-                    z = lagrange(obj.t(idxStart:idxEnd,9),obj.pos.(satsys){prnIdx}(idxStart:idxEnd,3),mtime(i)); 
+                    x(i,1) = lagrange(obj.t(idxStart:idxEnd,9),obj.pos.(satsys){prnIdx}(idxStart:idxEnd,1),mtime(i));
+                    y(i,1) = lagrange(obj.t(idxStart:idxEnd,9),obj.pos.(satsys){prnIdx}(idxStart:idxEnd,2),mtime(i)); 
+                    z(i,1) = lagrange(obj.t(idxStart:idxEnd,9),obj.pos.(satsys){prnIdx}(idxStart:idxEnd,3),mtime(i)); 
                 end
             else
                 error('Satellite %s not available in ephemeris!',prn);
