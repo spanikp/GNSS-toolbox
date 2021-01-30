b687f5bc23756f9fd5d3e187c7950b9b1d048cd8
spanikp <petospanik@gmail.com>
2021-01-30 11:12:03 +0100

    WIP on Azure pipelines devOps

--- test/runAllTests.m
+++ test/runAllTests.m
@@ -1,34 +1,33 @@
-classdef runAllTests
-    methods
-        function obj = runAllTests(obj)
-            import matlab.unittest.TestSuite
-            import matlab.unittest.TestRunner
-            import matlab.unittest.plugins.ToUniqueFile;
-            import matlab.unittest.plugins.TAPPlugin
-            import matlab.unittest.plugins.CodeCoveragePlugin
-            import matlab.unittest.plugins.codecoverage.CoberturaFormat
-            
-            addpath(genpath('../src'))
-            
-            % Create test suite
-            suite = TestSuite.fromFolder(pwd(),'IncludingSubfolders',true);
-            
-            % Run without test report
-            % run(suite)
-            
-            % Add plugin to create test report
-            runner = TestRunner.withTextOutput();
-            stream = ToUniqueFile('.','WithPrefix','testResultsReport','WithExtension','.tap');
-            pluginTestOutput = TAPPlugin.producingVersion13(stream);
-            runner.addPlugin(pluginTestOutput);
-            
-            % Add plugin to generate code coverage report
-            reportFormat = CoberturaFormat('testCoverageReport.xml');
-            pluginTestCoverage = CodeCoveragePlugin.forFolder('../src',...
-                'IncludingSubfolders',true,'Producing',reportFormat);
-            runner.addPlugin(pluginTestCoverage);
-            
-            runner.run(suite)
-        end
-    end
-end
+function results = runAllTests()
+import matlab.unittest.TestSuite
+import matlab.unittest.TestRunner
+import matlab.unittest.plugins.ToUniqueFile;
+import matlab.unittest.plugins.XMLPlugin
+import matlab.unittest.plugins.TAPPlugin
+import matlab.unittest.plugins.CodeCoveragePlugin
+import matlab.unittest.plugins.codecoverage.CoberturaFormat
+
+addpath(genpath('../src'));
+thisFolder = fileparts(mfilename('fullpath'));
+
+% Create test suite
+suite = TestSuite.fromFolder(pwd(),'IncludingSubfolders',true);
+
+% Create JUnit plugin
+xmlFile = fullfile(thisFolder,'junit_test_results.xml');
+pluginJUnitTestOutput = XMLPlugin.producingJUnitFormat(xmlFile);
+
+% Add JUnit plugin to create test report in XML format
+runner = TestRunner.withNoPlugins;
+runner.addPlugin(pluginJUnitTestOutput);
+
+% Add plugin to generate code coverage report
+reportFormat = CoberturaFormat(fullfile(thisFolder,'test_coverage_report.xml'));
+pluginTestCoverage = CodeCoveragePlugin.forFolder('../src',...
+    'IncludingSubfolders',true,'Producing',reportFormat);
+runner.addPlugin(pluginTestCoverage);
+
+% Running configured test suite
+results = runner.run(suite)
+
+
